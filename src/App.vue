74eb121274882cc134dd8956b6cd3c51cdbfb4c1
Görkem Başaran <200254061@ogr.alanya.edu.tr>
2023-12-14 03:59:17 +0300

    Load project

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <app-new-product></app-new-product>
+    <br><br>
+    <app-progress></app-progress>
+    <br><br>
+    
+    <app-products></app-products>
+
+  </div>
+
+
+</template>
+
+<script>
+  import NewProduct from './components/NewProduct.vue';
+  import Progress from './components/Progress.vue'
+  import Products from './components/Products.vue';
+
+  export default {
+    components: {
+      appNewProduct : NewProduct,
+      appProgress : Progress,
+      appProducts : Products
+    },
+    methods: {
+      onChange(e) {
+        const file = e.target.files[0];
+        this.product.selectedImage = URL.createObjectURL(file);
+      }
+    }
+  }
+</script>
+
+<style>
+  body {
+    background-color: aliceblue;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+
+  .card{
+    margin-right: 5px;
+    margin-bottom: 5px;
+  }
+
+  .card-img-top{
+    margin-top: 10px;
+  }
+
+  .card:last-child{
+    margin-right: 0px;
+  }
+
+  .col-md-2{
+    max-width: 15.666667%!important;
+  }
+
+  .product-container{
+    margin-left: 15px;
+  }
+
+</style>
+
